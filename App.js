ce32c60aee66abcddff19ba082a3bcbf5eed89f2
Erik Firdaus <erikfirdaus03@gmail.com>
2023-07-26 14:19:32 +0700

    menambahkan ritisan navigasi botom tab

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+  const BolaScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Text>Bola</Text>
+      </View>
+    );
+  };
+
+  const TrendingScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Text>Trending</Text>
+      </View>
+    );
+  };
+
+  const JalanTolScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Text>JalanTol</Text>
+      </View>
+    );
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Bola" component={BolaScreen} />
+        <Tab.Screen name="Trending" component={TrendingScreen} />
+        <Tab.Screen name="JalanTol" component={JalanTolScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
